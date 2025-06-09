94ed306846acb42027651d1a68ec8410749a9a41
dubeyaayushi <dubeyaayushi1028@gmail.com>
2025-06-09 14:52:17 +0530

    we have createdd our navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
 const App = () => {
 
 
@@ -10,6 +11,11 @@ const App = () => {
   return (
     <div>
      {!isOwnerPath && <Navbar/>}
+     <div className='min-h-[70vh]'>
+    <Routes>
+      <Route path ='/' element={<Home/>}/>
+    </Routes>
+     </div>
     </div>
   )
 }
