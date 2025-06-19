9774c4a6f698fcac07ee2a56ba5c16d0330f35ee
dubeyaayushi <dubeyaayushi1028@gmail.com>
2025-06-19 20:42:49 +0530

    4th deployement

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 /* creating our basic server */
-import "dotenv/config"/* using this we can use our environmental variables */
 
 import express from "express"
+import "dotenv/config"/* using this we can use our environmental variables */
 
 import cors  from "cors";/* cors will allow to connect any Backend with the frontend */
 // import { clerkMiddleware } from '@clerk/express'/* importing clerk middleware to use clerk authentication */
@@ -43,3 +43,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
 
+export default app; // Export the app for testing or further configuration
